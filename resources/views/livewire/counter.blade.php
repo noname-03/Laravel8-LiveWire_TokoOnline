f0a8b150be1a67b349dc773cce115d561c5918c3
noname-03 <fahrurrozi.arul3@gmail.com>
2021-08-22 16:18:46 +0700

    auth with vue dan livewire sederhanayaitu count

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    {{-- Because she competes with no one, no one can compete with her. --}}
+    {{-- <h1>hai</h1> --}}
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
+    <button wire:click="decrement">-</button>
+</div>
